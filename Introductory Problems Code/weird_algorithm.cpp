94f51969b88efa86197925195d36f3e58e445910
vladn <73899465+vladn79@users.noreply.github.com>
2023-06-29 14:27:54 +0300

    1 commit

---
+++ Introductory Problems Code/weird_algorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    long long int n;
+    std::cin >> n;
+    std::cout << n << " ";
+    while (n > 1)
+    {
+        if (n % 2 == 0){
+            n /= 2;
+        }
+
+        else {
+            n = (n * 3) + 1;
+        }
+        
+        std::cout << n << " ";
+
+    }
+    
+}
+
